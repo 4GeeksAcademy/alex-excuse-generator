c210d0938eccbabf47591897a9d6d485751d93c3
Alexander Morales <alexandermorales.n1@gmail.com>
2025-03-20 15:00:31 +0000

    add div in html and create excuse function

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let htmlTxt = document.querySelector('#excuse')
+const randomNum2 = function(){
+  let num = Math.floor(Math.random() * 3) 
+  return num;
+}
+const randomNum3 = function(){
+  let num = Math.floor(Math.random() * 4)
+  return num;
+}
+const randomNum4 = function(){
+  let num = Math.floor(Math.random() * 5)
+  return num;
+}
+htmlTxt.innerHTML = `${who[randomNum3()]} ${action[randomNum3()]} ${what[randomNum2()]} ${when[randomNum4()]}`
 };
